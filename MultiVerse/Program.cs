91b3685ccc46a14a7d858e4cd7f09271cb089cdc
Lagrange Laura <laura.lagrange74@gmail.com>
2023-03-17 10:38:14 +0100

    first commit

---
+++ MultiVerse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiVerse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Master!");
+        }
+    }
+}
